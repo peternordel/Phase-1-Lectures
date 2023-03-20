6ccd9cf33f0d0408f9419c4a635cac8edc20b306
stephenlambertfs <stephenolambert@gmail.com>
2023-03-19 21:11:32 -0600

    Added comments for lecture

---
+++ index.js
@@ -0,0 +1,76 @@
+// Functions consists statements/tasks that return a value or undefined
+
+
+
+// Function declaration
+//Create a greeting function that returns
+
+
+// Function expression
+//To invoke a function call its name and use parenthesis
+
+
+
+//create a greeting function that console logs a greeting
+
+
+//console.log and return do different things
+//console.log only logs something to the console
+//return only returns the value of the function
+
+
+//Function can take variables called parameters
+//When the function is called, it is passed arugments that become the value of the parameter
+
+
+//Create a function called squareNum
+//that takes one parameter and returns the squared number of that parameter
+
+
+//Function can take multiple parameters
+//Create a function called addNums, that takes two parameters and returns the sum of those two parameters
+
+
+//Whats another way to write a function?
+
+
+
+
+//Arrow functions
+// Arrow functions are shorter to write
+// Arrow functions with a single parameter do not need parenthesis
+//The benefits of functions are that they are reusable and can be called anywhere in your code
+
+
+//create a arrow function that takes one parameter and returns your favorite food, assign it to a variable
+
+
+//Functions are just like variables
+//They can be passed into other functions, assigned to variables, and returned from other functions
+//This makes them first class functions
+
+
+
+// A function that returns a function is called a Higher-Order Function.
+//create a funtion that returns a function
+
+
+
+//A function that is taken as an argument is a callback
+//create a greeting function, that take a callback as an argument and returns the callback
+
+
+
+//Create a function called max that takes two numbers as arguments 
+//the function should return the a string with the largest number
+//if the numbers are equal, return a string that says the numbers are equal
+
+
+
+
+
+
+
+
+
+
