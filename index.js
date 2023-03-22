72689e2ab36da2225236472f4d75a729c35675f5
stephenlambertfs <stephenolambert@gmail.com>
2023-03-22 10:51:50 -0600

    added soltions for objects

--- index.js
+++ index.js
@@ -3,12 +3,97 @@
 //Review objects
 //An object is a collection of data, that contains a list of properties (key value pairs) 
 //in curly brackets
+const obj = {}
+const userInfo = {
+  firstName: "Avi",
+  lastName: "Flombaum",
+  companyName: "Flatbook Labs",
+  jobTitle: "Developer Apprentice",
+  friend1firstName: "Nancy",
+  friend1lastName: "Burgess",
+  friend1companyName: "Flatbook Labs",
+  friend1jobTitle: "Developer Apprentice",
+  friend2firstName: "Corinna",
+  friend2lastName: "Jackson",
+  friend2companyName: "Flatbook Labs",
+  friend2jobTitle: "Senior Developer",
+  project1title: "Flatbook",
+  project1description:
+    "The premier Flatiron School-based social network in the world.",
+  project2title: "Scuber",
+  project2description:
+    "A burgeoning startup helping busy parents transport their children to and from all of their activities on scooters.",
+};
 
-//Define a nested object 
+console.log(userInfo.firstName)
 
+//Iterate through an object using the for in loop
+
+function showUserInfo(){
+  for(var key in userInfo){
+    console.log(userInfo[key])
+  }
+}
+showUserInfo()
+
+//Create a nested object, iterate and select data 
+const userInfo2 = {
+  firstName: "Avi",
+  lastName: "Flombaum",
+  company: {
+    name: "Flatbook Labs",
+    jobTitle: "Developer Apprentice",
+  },
+  friends: [
+    {
+      firstName: "Nancy",
+      lastName: "Burgess",
+      company: {
+        name: "Flatbook Labs",
+        jobTitle: "Developer Apprentice",
+      },
+    },
+    {
+      firstName: "Corinna",
+      lastName: "Jackson",
+      company: {
+        name: "Flatbook Labs",
+        jobTitle: "Lead Developer",
+      },
+    },
+  ],
+  projects: [
+    {
+      title: "Flatbook",
+      description:
+        "The premier Flatiron School-based social network in the world.",
+    },
+    {
+      title: "Scuber",
+      description:
+        "A burgeoning startup helping busy parents transport their children to and from all of their activities on scooters.",
+    },
+  ],
+};
 //Demostrate selecting data from the object using brackets
-
-//Create a nested object, iterate and select data
+console.log(userInfo2.company.name)
+console.log(userInfo2.friends[0].company.name)
+console.log(userInfo2.projects[0].title)
+//iterate through the object using a for in loop
+function showUserInfo2(){
+  for(var key in userInfo2){
+    console.log(userInfo2[key])
+  }
+}
+console.log(showUserInfo2())
+//iterate through the object using a for in loop, 
+//create a function that takes the data and displays the taget
+function showUserInfo3(target){
+  for(var key in target){
+    console.log(target[key])
+  }
+}
+showUserInfo3(userInfo2.company)
 
 const pokemon = [
   {
@@ -52,7 +137,7 @@ pokeForm.addEventListener("submit", function (e) {
   const img = document.querySelector("#img-input").value;
 
   let newChar = {
-    id: 6, // NEEDS TO CHANGE,
+    id: 6,
     name: name,
     img: img,
     likes: 0,
@@ -97,7 +182,7 @@ function renderPokemon(char) {
   const deleteBtn = document.createElement("button");
   deleteBtn.className = "delete-bttn";
   deleteBtn.textContent = "Delete";
-  deleteBttn.addEventListener("click", function () {
+  deleteBtn.addEventListener("click", function () {
     pokeCard.remove();
   });
 
