55e030f1ec1cc749e01fb564d994a71b521b89fc
stephenlambertfs <stephenolambert@gmail.com>
2023-03-23 11:05:26 -0600

    added comments in the starter file

--- index.js
+++ index.js
@@ -1,141 +1,17 @@
-//Review developer tools
-
-//Review objects
-//An object is a collection of data, that contains a list of properties (key value pairs) 
-//in curly brackets
-const obj = {}
-const userInfo = {
-  firstName: "Avi",
-  lastName: "Flombaum",
-  companyName: "Flatbook Labs",
-  jobTitle: "Developer Apprentice",
-  friend1firstName: "Nancy",
-  friend1lastName: "Burgess",
-  friend1companyName: "Flatbook Labs",
-  friend1jobTitle: "Developer Apprentice",
-  friend2firstName: "Corinna",
-  friend2lastName: "Jackson",
-  friend2companyName: "Flatbook Labs",
-  friend2jobTitle: "Senior Developer",
-  project1title: "Flatbook",
-  project1description:
-    "The premier Flatiron School-based social network in the world.",
-  project2title: "Scuber",
-  project2description:
-    "A burgeoning startup helping busy parents transport their children to and from all of their activities on scooters.",
-};
-
-console.log(userInfo.firstName)
-
-//Iterate through an object using the for in loop
-
-function showUserInfo(){
-  for(var key in userInfo){
-    console.log(userInfo[key])
-  }
-}
-showUserInfo()
-
-//Create a nested object, iterate and select data 
-const userInfo2 = {
-  firstName: "Avi",
-  lastName: "Flombaum",
-  company: {
-    name: "Flatbook Labs",
-    jobTitle: "Developer Apprentice",
-  },
-  friends: [
-    {
-      firstName: "Nancy",
-      lastName: "Burgess",
-      company: {
-        name: "Flatbook Labs",
-        jobTitle: "Developer Apprentice",
-      },
-    },
-    {
-      firstName: "Corinna",
-      lastName: "Jackson",
-      company: {
-        name: "Flatbook Labs",
-        jobTitle: "Lead Developer",
-      },
-    },
-  ],
-  projects: [
-    {
-      title: "Flatbook",
-      description:
-        "The premier Flatiron School-based social network in the world.",
-    },
-    {
-      title: "Scuber",
-      description:
-        "A burgeoning startup helping busy parents transport their children to and from all of their activities on scooters.",
-    },
-  ],
-};
-//Demostrate selecting data from the object using brackets
-console.log(userInfo2.company.name)
-console.log(userInfo2.friends[0].company.name)
-console.log(userInfo2.projects[0].title)
-//iterate through the object using a for in loop
-function showUserInfo2(){
-  for(var key in userInfo2){
-    console.log(userInfo2[key])
-  }
-}
-console.log(showUserInfo2())
-//iterate through the object using a for in loop, 
-//create a function that takes the data and displays the taget
-function showUserInfo3(target){
-  for(var key in target){
-    console.log(target[key])
-  }
-}
-showUserInfo3(userInfo2.company)
-
-const pokemon = [
-  {
-    id: 1,
-    name: "bulbasaur",
-    img: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
-    likes: 4,
-  },
-  {
-    id: 2,
-    name: "ivysaur",
-    img: "https://images.cults3d.com/6VgkTLM1j-CTAMhEJTtsRV1z6N8=/516x516/https://files.cults3d.com/uploaders/14845535/illustration-file/5d09c257-51ed-4d65-aa36-3f9201af34c4/ivysaur.png",
-    likes: 21,
-  },
-  {
-    id: 3,
-    name: "venusaur",
-    img: "https://images.saymedia-content.com/.image/t_share/MTc2MjYwODQ5NTk2NTcyODYy/pokemon-venusaur-nicknames.png",
-    likes: 7,
-  },
-  {
-    id: 4,
-    name: "charmander",
-    img: "https://pixy.org/download/1207107/",
-    likes: 20,
-  },
-  {
-    id: 5,
-    name: "charmeleon",
-    img: "https://static.pokemonpets.com/images/monsters-images-800-800/5-Charmeleon.webp",
-    likes: 11,
-  },
-];
-
 const pokeContainer = document.querySelector("#poke-container");
 const pokeForm = document.querySelector("#poke-form");
+
+//TODO: Create the getPokemon function
+//Makes a 'GET' request to http://localhost:3000/characters and receives a response of all the characters
+//Invokes renderPokemon() for each returned character
+//break the problem down into steps
 
 pokeForm.addEventListener("submit", function (e) {
   e.preventDefault();
   const name = document.querySelector("#name-input").value;
   const img = document.querySelector("#img-input").value;
 
+  //create a new character with the udpated id, name, img, and likes
   let newChar = {
     id: 6, // Will need to change this if we add more characters
     name: name,
@@ -151,10 +27,20 @@ pokemon.forEach(function (character) {
   renderPokemon(character);
 });
 
+
+//TODO: Create the showCharacter function
+//Make a 'GET' request for a single pokemon character data via `http://localhost:3000/characters/:id`
+//Invoke renderPokemon() for the returned character
+//Update the id of the card returned by renderPokemon() to 'poke-show-card'
+//Replace `pokeContainer` innerHTML with the matched character only. HINT: use `.replaceChildren()`
+//break the problem down into steps
+
+
 function renderPokemon(char) {
   const pokeCard = document.createElement("div");
   pokeCard.className = "poke-card";
 
+  //TODO: Create an event listener for the pokeCard div, that invokes the showCharacter function
   const pokeImg = document.createElement("img");
   pokeImg.src = char.img;
   pokeImg.alt = `${char.name} image`;
@@ -173,6 +59,8 @@ function renderPokemon(char) {
   likesBttn.className = "like-bttn";
   likesBttn.textContent = "♥";
   likesBttn.addEventListener("click", function () {
+    //use e.stopPropogation() to stop the event from bubbling up to the parent element
+
     // increment the characters number of likes
     ++char.likes;
     // update the DOM to reflect the new number of likes
@@ -183,6 +71,7 @@ function renderPokemon(char) {
   deleteBtn.className = "delete-bttn";
   deleteBtn.textContent = "Delete";
   deleteBtn.addEventListener("click", function () {
+    //use e.stopPropogation() to stop the event from bubbling up to the parent element
     pokeCard.remove();
   });
 
